0096f717d611dc3afab77c6fd7176823780ee34f
nathaliafelix31 <nathy.felix.31@gmail.com>
2022-09-07 12:49:40 -0300

    add: Autenticação e jsonwebtoken

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(bodyParser.json()); //para entender quando enviar uma aquisição em json
 app.use(bodyParser.urlencoded({extended: false})) //para entender paramentos passados na url para decodá-los
 
-/*app.get('/', (req, res)=>{ //req = dados da requisição res= objeto utilizafo para enviar pro usuario quando utilizar a rota
-    res.send('OK');
-});*/
+
 require('./controllers/authController')(app);
-app.listen(3000); //porta que quero ouvir
+require('./controllers/projectController')(app);
+app.listen(3000); 
